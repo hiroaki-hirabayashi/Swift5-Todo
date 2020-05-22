d15f5caa33d70a848dab5c3ad242e99e01aac786
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-05-22 21:48:21 +0900

    UI調整、データベース登録の雛形を作成しました。

---
+++ Swift5 Todo/TodoTableViewController.swift
@@ -0,0 +1,129 @@
+//
+//  TodoTableViewController.swift
+//  Swift5 Todo
+//
+//  Created by 平林宏淳 on 2020/05/22.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+
+    
+    //withIdentifierを設定した名前に合わせる
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+//         Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBAction func tapAddButton(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Todoを追加しますか？", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "追加", style: .default){
+            (void) in
+            let textField = alertController.textFields![0] as UITextField
+            if let text = textField.text {
+                
+                print("データベースに登録します。")
+            }
+            
+        }
+        
+        let cancel = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        
+        alertController.addTextField{(textField) in
+            textField.placeholder = "Todoの名前を入れてください。"
+            
+        }
+        alertController.addAction(action)
+        alertController.addAction(cancel)
+        
+        present(alertController, animated: true, completion: nil)
+
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
