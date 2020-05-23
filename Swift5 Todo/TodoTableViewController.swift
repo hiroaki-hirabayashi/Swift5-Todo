8ca583d8d59e62971788566395cd013e6c6ccf6a
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-05-23 23:35:04 +0900

    データ登録、削除、並べ替え機能を実装しました。

--- Swift5 Todo/TodoTableViewController.swift
+++ Swift5 Todo/TodoTableViewController.swift
@@ -7,30 +7,35 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TodoTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
     }
     
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+
         return 1
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 10
+        //Realmをインスタンス化して使えるようにする
+        let realm = try! Realm()
+        let todos = realm.objects(Todo.self)
+        
+        return todos.count
     }
 
     
@@ -39,45 +44,64 @@ class TodoTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
 //         Configure the cell...
-
+        
+        let realm = try! Realm()
+        let todos = realm.objects(Todo.self)
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.text
         return cell
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
+    //データ削除設定
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            let realm = try! Realm()
+            let todos = realm.objects(Todo.self)
+            let todo = todos[indexPath.row]
+            
+            try! realm.write {
+                realm.delete(todo)
+            }
+            
+            
+            
+            
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+        }
     }
-    */
+    
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    */
+    
 
-    /*
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+//
+//    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+//         return .none
+//    }
+
+   
 
     /*
     // MARK: - Navigation
@@ -88,6 +112,8 @@ class TodoTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
 
     
     @IBAction func tapAddButton(_ sender: Any) {
@@ -98,7 +124,18 @@ class TodoTableViewController: UITableViewController {
             let textField = alertController.textFields![0] as UITextField
             if let text = textField.text {
                 
-                print("データベースに登録します。")
+                let todo = Todo()
+                todo.text = text
+                
+                //Realmをインスタンス化して使えるようにする
+                let realm = try! Realm()
+                // Persist your data easily 永続化
+                try! realm.write {
+                    realm.add(todo)
+                }
+                
+                self.tableView.reloadData()
+                
             }
             
         }
